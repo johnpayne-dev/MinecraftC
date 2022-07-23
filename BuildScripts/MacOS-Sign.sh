6d4d097fcee3198e280f45f57f4256045a4e01b3
John Payne <email@johnpayne.dev>
2022-07-23 15:53:17 -0500

    refactor scripts

--- BuildScripts/MacOS-Sign.sh
+++
@@ -1,7 +0,0 @@
-xattr -cr Output/MinecraftC.app
-codesign -s "$1" Output/MinecraftC.app/Contents/MacOS/MinecraftC
-if [[ $# == 3 ]]; then
-	editto -c -k --keepParent Output/MinecraftC.app Output/MinecraftC-MacOS.zip
-	xcrun altool --notarize-app --primary-bundle-id "$2" -u "$3" --file Output/MinecraftC-MacOS.zip
-	xcrun stapler staple Output/MinecraftC.app
-fi
